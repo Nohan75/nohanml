e264e2f35a694686147f352dad58e5d158eb584b
Nohan75 <nohanmarielouise@gmail.com>
2026-04-04 16:26:55 +0200

    feat: replace loading text with skeleton screens and fade-in animations

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProjectCardComponent } from '../../core/components/project-card/project-card.component';
+import { SkeletonCardComponent } from '../../core/components/skeleton-card/skeleton-card.component';
 import { ProjectsStore } from '../../store/projects.store';
 import { ProfileStore } from '../../store/profile.store';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, ProjectCardComponent],
+  imports: [RouterLink, ProjectCardComponent, SkeletonCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
